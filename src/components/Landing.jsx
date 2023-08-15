b3cc7d29cf5a2a52713cccc5d97f2415d2140068
Anabel-Espinoza <anabel.espinoza@gmail.com>
2023-08-15 10:54:05 -0400

    images path changes

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -3,7 +3,6 @@ import useMediaQuery from "../hooks/useMediaQuery";
 import { motion } from "framer-motion";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
-
 const Landing = ({ setSelectedPage }) => {
   const isAboveLarge = useMediaQuery("(min-width: 1060px)");
   return (
